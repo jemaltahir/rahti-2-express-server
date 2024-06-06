7069f32c0493c4b4afbe68a44a095b2ed2418152
Tahir Jemal <jemalmtahir@gmail.com>
2024-06-06 16:04:22 +0300

    Add MySQL connection

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # rahti-2-express-server
 Testing express server to connect MySQL in Rahti2.
+
+##
+```
+npm init -y
+
+npm install mysql
+```
+
+Run the following commands to set the environment variables:
+
+```
+oc set env dc/my-express-app MYSQL_HOST=<mysql-service>
+oc set env dc/my-express-app MYSQL_USER=<mysql-username>
+oc set env dc/my-express-app MYSQL_PASSWORD=<mysql-password>
+oc set env dc/my-express-app MYSQL_DATABASE=<mysql-database>
---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// MySQL connection
+const db = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL');
+});
+
+app.get('/', (req, res) => {
+  db.query('SELECT NOW()', (err, result) => {
+    if (err) {
+      res.status(500).send('Error querying database');
+      return;
+    }
+    res.send(`Database time: ${result[0]['NOW()']}`);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
